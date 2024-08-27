f60f302c031866a3432586df0abb17318fcde440
Arlene <arlenesgo1998@gmail.com>
2024-08-27 13:12:29 -0700

    Chat finalizado

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -18,21 +18,22 @@ io.on('connection', (client) => {
         let personas = usuarios.agregarPErsona(client.id, data.nombre, data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
-
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
         // callback(personas);
         callback(usuarios.getPersonasPorSala(data.sala));
         // console.log(usuario);
     })
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.emit('crearMensaje', mensaje);
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
         let personaBorrada = usuarios.borrarPersona(client.id);
-        client.broadcast.to(personaBorrada.sala).emit('creaMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
         client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
     });
     //Mensajes privados
